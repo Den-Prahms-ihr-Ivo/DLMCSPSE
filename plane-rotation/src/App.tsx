2469d2e003823abbd7a16860aabdb1f1e84a506b
Den-Prahms-ihr-Ivo <ivo@ivolino.de>
2025-01-03 10:33:27 +0100

    Most Basic Layout

---
+++ plane-rotation/src/App.tsx
@@ -0,0 +1,26 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="red.400">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="blue.500">
+        Main
+      </GridItem>
+    </Grid>
+  );
+}
+
+export default App;
